dc98b62c9e91bd99842392cc30c9f0e8b5250854
mukaweb <muktarhassen6@gmail.com>
2025-03-12 08:47:24 +0300

    Remove hire button from ProjectCard component

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -90,9 +90,9 @@ export function ProjectCard({ project, index }) {
                         </div>
 
                         {/* Hire button */}
-                        <button className="px-4 py-1.5 text-sm font-medium text-gray-700 dark:text-gray-200 border border-gray-200 dark:border-gray-700 rounded-full hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors">
+                        {/* <button className="px-4 py-1.5 text-sm font-medium text-gray-700 dark:text-gray-200 border border-gray-200 dark:border-gray-700 rounded-full hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors">
                             Hire {project.developer?.name || 'developer'}
-                        </button>
+                        </button> */}
                     </div>
                 </div>
             </Link>
